eb3336463db073c80380926f440e29967fb504a5
Akash Patil <akashpatilsrt@gmail.com>
2021-09-30 09:14:40 +0530

    Adding TiTacToegame

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -5,7 +5,10 @@ public class TicTacToeGame {
 
 
     }
-    public static void board(){
+    /*   creating char array board
+
+     */
+    public static void Createboard(){
         char[] board = {' ','0','0','0','0','0','0','0','0','0'};
         for(int i=1;i<board.length;i++){
             board[i]=' ';
