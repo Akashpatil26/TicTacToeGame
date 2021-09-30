7d54f6bfd9999d0e12c05e24cac522c652efe8b0
Akash Patil <akashpatilsrt@gmail.com>
2021-09-30 10:18:29 +0530

    Adding TicTacToeGame

--- src/Tictactoegame/TicTacToegame.java
+++ src/Tictactoegame/TicTacToegame.java
@@ -3,19 +3,13 @@ package Tictactoegame;
 public class TicTacToegame {
     public static void main(String[] args) {
         System.out.println("Welcome to the TicTacToeGame");
+        createBoard();
 
     }
 
-    char[] board = {' ','0','0','0','0','0','0','0','0','0'};
-    /*   creating char array board
-
-     */
-
-    public static void Createboard(){
-        /*char[] board = {' ','0','0','0','0','0','0','0','0','0'};/*
-
-         */
-        char[]board = new char[10];
+    /*   creating char array board*/
+    public static void createBoard(){
+        char[] board = new char[10];
         for(int i=1;i<board.length;i++){
             board[i]=' ';
         }
