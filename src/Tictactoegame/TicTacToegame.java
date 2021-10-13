4ee6882d6101f860b9d0be269ca34b9c56b6a00a
Akash Patil <akashpatilsrt@gmail.com>
2021-10-13 20:04:01 +0530

    Added UC3

--- src/Tictactoegame/TicTacToegame.java
+++ src/Tictactoegame/TicTacToegame.java
@@ -3,12 +3,13 @@ package Tictactoegame;
 import java.util.Random;
 import java.util.Scanner;
 
-public class TicTacToegame {
+public class TicTacToegame{
+
+    char [][] board = new char[3][3];
     public static void main(String[] args) {
         System.out.println("Welcome to the TicTacToeGame");
         createBoard();
         inputLetter();
-
     }
 
     /*   creating char array board*/
@@ -22,6 +23,13 @@ public class TicTacToegame {
         Scanner scanner=new Scanner(System.in);
         System.out.println("Enter X or O :");
         String input=scanner.next();
+    }
+    public static void showBoard(char[][] board){
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                System.out.println(board[i][j]);
+            }
+        }
     }
 
 
