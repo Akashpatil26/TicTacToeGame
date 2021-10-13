feb10d54d36ba045fa678b5c9b275a80a6fb6a54
Akash Patil <akashpatilsrt@gmail.com>
2021-10-13 22:07:36 +0530

    Added UC4

--- src/Tictactoegame/TicTacToegame.java
+++ src/Tictactoegame/TicTacToegame.java
@@ -4,12 +4,32 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class TicTacToegame{
-
     char [][] board = new char[3][3];
+  //  TicTacToegame ticTacToegame = new TicTacToegame();
+
     public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
         System.out.println("Welcome to the TicTacToeGame");
         createBoard();
         inputLetter();
+        char [][] board = new char[3][3];
+        int row =0;
+        int column =0;
+        /*getting row and column from user
+         */
+        System.out.println("Enter a row (0,1,2): ");
+        row=scanner.nextInt();
+        System.out.println("Enter a row (0,1,2): ");
+        column=scanner.nextInt();
+        if (row <0 || column <0 || row >2 || column >2){
+            System.out.println("Invalid Number ,Enter 0,1,2");
+        else if(board[row][column] !='-'){
+                System.out.println("A move has been already made ");
+            }
+        else {
+
+            }
+        }
     }
 
     /*   creating char array board*/
@@ -27,10 +47,12 @@ public class TicTacToegame{
     public static void showBoard(char[][] board){
         for(int i=0;i<3;i++){
             for(int j=0;j<3;j++){
-                System.out.println(board[i][j]);
+                System.out.println(board[i][j]=' ');
             }
+            System.out.println();
         }
     }
+
 
 
 
